0d131c7a472995effcb465ac83d94538f41a1881
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-29 00:16:48 +0530

    Added: MyBinaryNode file

---
+++ MyBinaryNode.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+/*
+ * Author:Prasad
+ */
+public class MyBinaryNode<K extends Comparable<K>> {
+	//variables
+	K key;
+	MyBinaryNode<K> left;
+	MyBinaryNode<K> right;
+	//constructor
+	public MyBinaryNode(K key) {
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}	
+}
