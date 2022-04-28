9eb3e2d83605d6244112a3ee44c8d13e5ccf68c1
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-29 00:51:49 +0530

    Added: Ability to search 63 in binary tree

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -47,7 +47,33 @@ class MyBinaryTreeTest {
 		myBinaryTree.add(67);
 		int size = myBinaryTree.getSize();
 		//Test condition
-		Assert.assertEquals(13, size);
+		Assert.assertEquals(13, size);	
+	}
+	
+	/*
+	 * Ability to search 63 in binary tree
+	 */
+	@Test
+	public void search63InBinaryTree() {
+		//object
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		//calling add method
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(60);
+		myBinaryTree.add(40);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 		
+		boolean result = myBinaryTree.searchTree(63);
+		//test condition
+		Assert.assertTrue(result);
 	}
 }
