82eb245e7966b0e6d1301fc99a3606a13b07e7c0
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-29 00:27:31 +0530

    Added: Ability to create binary tree

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -23,4 +23,31 @@ class MyBinaryTreeTest {
 		//test condition
 		Assert.assertEquals(3, size);
 	}
+	
+	/*
+	 * Ability to create binary tree
+	 */
+	@Test
+	public void given13NumbersWhenAddedToBinaryTreeShouldReturnSizeThirteen() {
+		//object
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		//calling add method
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(60);
+		myBinaryTree.add(40);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		//Test condition
+		Assert.assertEquals(13, size);
+		
+	}
 }
